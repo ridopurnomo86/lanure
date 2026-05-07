18ca6296c5b1afa080a1479bf5caf648083411bc
Ridho Purnomo <ridopurnomo86@gmail.com>
2026-05-08 01:35:21 +0700

    fix:environtment fail

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,5 +2,13 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  if (process.env.NODE_ENV === "production") {
+    console.error("❌ DATABASE_URL is not set!");
+  }
+}
+
+const sql = neon(databaseUrl || "");
 export const db = drizzle(sql, { schema });
